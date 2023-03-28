892f460a47ce3a105521952a57992b9c4db1cfc6
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-03-27 20:09:09 -0400

    desvirar cartas difrentes
    lockboard serve pra evitar bug

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const cards = document.querySelectorAll('.card')
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if(lockBoard) return;
     this.classList.add('flip')
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -20,7 +22,16 @@ function checkForMath(){
     }
     unFlipCards()
 }
-
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+function unFlipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+    }, 1500)
+}
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
