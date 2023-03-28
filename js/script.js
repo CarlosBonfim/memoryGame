aba6e9af030b6fa4546cc5bfd1f44ebfb6b824c6
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-03-27 20:22:05 -0400

    tratamento de duplo clique

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
+    if(this === firstCard) return;
     this.classList.add('flip')
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -25,13 +26,23 @@ function checkForMath(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
 function unFlipCards(){
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+        // lockBoard = false;
+        resetBoard();
     }, 1500)
 }
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
